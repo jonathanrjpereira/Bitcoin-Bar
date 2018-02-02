3d0b5c89319afe377adeb4fda13ed0e8c3476f8e
jonathanrjpereira <jonathanrjpereira@gmail.com>
2018-02-02 13:44:42 +0000

    Easy Displaying of Data Paramters. Contains a list who's elements contain the name & value of data parameters. Upto 19 different data parameters can be displayed.

--- bcbar.py
+++ bcbar.py
@@ -1,7 +1,8 @@
-# py -2 bcbar.py
+# py -2 bcbar.py 
 
+# Web Scraping Prerequisites
 import requests
-
+# LED Matrix Prerequisites
 import re
 import time
 import argparse
@@ -13,41 +14,35 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 
-page = requests.get("http://www.bitcoinblockhalf.com/")
-#print (page.status_code)
-#print (page.content)
+while(1):
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(page.content, 'html.parser')
+    page = requests.get("http://www.bitcoinblockhalf.com/")
 
-#print(soup.prettify())
-#list(soup.children)
-#print([type(item) for item in list(soup.children)])
-
-#print(soup.find_all('td'))
-
-#for paragraph in soup.find_all('td'):
-#    print (paragraph.string)
-
-data = []
-for paragraph in soup.find_all('td'):
-    data.append(paragraph.string)
-
-disp = [0]*38
-for x in range(len(data)):
-    if x % 2 == 0:
-        disp[x] = data[x] + data[x+1].replace(',','')
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(page.content, 'html.parser') #Scrapes entire HTML file
 
 
+    data = []
+    for paragraph in soup.find_all('td'):   #Search for all values of td elements
+        data.append(paragraph.string)
 
-#coins_2b_mined = (data[7].replace(',','')) #data[1] is the Coins remaining to be Mined. replace() removes the ',' from the string. int() coverts the string into an into
-#coins_2b_mined = data[7]  #Valuw with commas does not look neat on Dot Matrix display.
+
+    disp = [0]*38 #38 is the length of the list data
+    for x in range(len(data)):
+        if x % 2 == 0:
+            disp[x] = data[x] + " " + data[x+1].replace(',','') #Each element of disp is the Name of the parameter & its value. The commas present in the values have been removed for better displaying asthetics
+    #       disp[x] = data[x] + " " + data[x+1] #Display with commas
+            
+    disp = list(filter(lambda a:a != 0, disp)) #For some reason every odd element of the list 'disp' is '0'. This removes all occurences of '0' from the list 'disp'
+    #Remove 'list' in Python2.7
 
 
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=4 , block_orientation=-90, rotate=2)
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=4 , block_orientation=-90, rotate=2)
 
-show_message(device, data[6] + coins_2b_mined, fill="white", font=proportional(LCD_FONT),scroll_delay = 0.04)
+    for i in range(len(disp)):
+        show_message(device, disp[i], fill="white", font=proportional(LCD_FONT),scroll_delay = 0.02) #Change the value of 'scroll_delay' to change the Scrolling Speed
+        
+    #show_message(device, disp[4], fill="white", font=proportional(LCD_FONT),scroll_delay = 0.02) # '4' indicates Displays the number of Bitcoins left to mine.
+    #Change this value according to the table to display various data parameters
 
-##with canvas(device) as draw:
-##    text(draw, (0, 0), msg, fill="white", font=proportional(LCD_FONT))
