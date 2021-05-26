7399281bc9bc15c2e69df33dabbf55cc80e88a6c
Tobias <t.lie-atjam@effytool.nl>
2021-05-26 20:05:59 +0200

    use locale to format price

--- models/coin.py
+++ models/coin.py
@@ -1,6 +1,6 @@
-#import locale
+import locale
 from decimal import Decimal, ROUND_HALF_UP
-#locale.setlocale( locale.LC_ALL, '' )
+locale.setlocale( locale.LC_ALL, '' )
 
 class Coin:  
     def __init__(self, id, rank, symbol, name, supply, maxSupply, marketCapUsd, volumeUsd24Hr, priceUsd, changePercent24Hr, vwap24Hr, explorer): 
@@ -14,7 +14,7 @@ class Coin:
         self.volumeUsd24Hr = volumeUsd24Hr
         self.price = Decimal(priceUsd)
         self.priceRounded = self.price.quantize(Decimal(".01"), rounding=ROUND_HALF_UP)
-        self.priceString = str(Decimal(self.priceRounded))
+        self.priceString = locale.currency(self.priceRounded), grouping=True, symbol=False)
         self.percentage = Decimal(changePercent24Hr)
         
         
